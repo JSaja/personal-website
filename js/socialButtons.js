3d3e2640d89b140a3cfa1d16b0af287330ed89d6
JSaja <sajax003@umn.edu>
2020-12-14 23:15:35 -0600

    Pushing changes
    Pushing changes on 12/14 progess:
    - Improved banner picture color temp
    - implemented beautiful gradient background based on picture
    - Added reactive, interactive social icons
    - Made tactile cards for homepage elements

---
+++ js/socialButtons.js
@@ -0,0 +1,17 @@
+function linkedinClick () {
+    var url = "https://linkedin.com/in/john-saja/";
+    var linkedin = document.querySelector("#linkedin");
+    window.open(url);
+}
+
+function githubClick () {
+    var url = "https://github.com/JSaja/";
+    var github = document.querySelector("#github");
+    window.open(url);
+}
+
+function facebookClick() {
+    var url = "https://facebook.com/john.saja.3";
+    var twitter = document.querySelector("#facebook");
+    window.open(url);
+}
