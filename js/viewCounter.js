e282e1aa311410ed7cf74af6b817547a5b9674b9
John <sajax003@umn.edu>
2020-12-27 13:31:00 -0600

    Test preflight request

--- js/viewCounter.js
+++ js/viewCounter.js
@@ -3,13 +3,32 @@
 console.log("Hello world from viewCounter.js");
 
 window.onload = function() {
+    // apiUrl = 'https://e8wa6k3qf5.execute-api.us-east-2.amazonaws.com/Prod';
     //Call API
-    fetch('https://e8wa6k3qf5.execute-api.us-east-2.amazonaws.com/Prod')
-    .then(response => {
-        console.log("API RESPONSE\n");
-        console.log(response.json);
-        return response.json();
-    });
+    // fetch('https://e8wa6k3qf5.execute-api.us-east-2.amazonaws.com/Prod')
+    // .then(response => {
+    //     console.log("API RESPONSE\n");
+    //     console.log(response.json);
+    //     return response.json();
+    // });
+
+    // fetch(
+    //     'https://e8wa6k3qf5.execute-api.us-east-2.amazonaws.com/Prod')
+    //     .then(response => response.json())
+    //     .then(data => console.log(data));
+
+    function callAPI() {
+        const xhttp = new XMLHttpRequest();
+        const apiUrl = 'https://e8wa6k3qf5.execute-api.us-east-2.amazonaws.com/Prod';
+        xhttp.open("GET", apiUrl);
+        xhttp.send();
+
+        xhttp.onreadystatechange=(e)=>{
+            console.log("TEST" + xhttp.responseText);
+        }
+    }
+
+    callAPI();
 
 
     //Retrieve value
